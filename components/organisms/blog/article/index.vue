b5605ff20525232f47e64e3010ac6bca710d9ddf
Alejandro Cano <alejandroyunes@outlook.com>
2024-08-24 17:08:46 -0500

    adding articl

---
+++ components/organisms/blog/article/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import './article.scss'
+</script>
+
+<template>
+  <section class="article">
+      <div class="title">
+        <h1>Next.js 14 App con Style X</h1>
+      </div>
+
+      <div class="content">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+        </p>
+      </div>
+
+      <div class="meta-box">
+        <div class="author">
+          <img src="" alt="Author">
+          <p>John Doe</p>
+        </div>
+      </div>
+
+  </section>
+</template>
---
+++ pages/blog/next-14-app-con-style-x/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Title from '~/components/atoms/title-description/index.vue'
+import BlogArticle from '~/components/organisms/blog/article/index.vue'
+
+useSeoMeta({
+  title: 'Blog de Vanguardia: Innovaciones en Tecnología y Marketing Digital',
+  ogTitle: 'Blog de Vanguardia: Innovaciones en Tecnología y Marketing Digital',
+  description: 'Explora nuestros últimos artículos sobre tecnología, desarrollo web y marketing digital. Aprende sobre Next.js, servidores modernos, desarrollo web y más.',
+  ogDescription: 'Descubre las últimas tendencias en tecnología y marketing digital con los artículos del blog de Vanguardia. Desde Next.js y desarrollo web hasta servidores modernos y Linux.',
+  ogImage: '/meta/inicio-web-profesional.webp',
+  ogLocale: 'es_CO',
+  twitterTitle: 'Blog de Vanguardia: Innovaciones en Tecnología y Marketing Digital',
+  twitterCard: 'summary_large_image',
+  twitterDescription: 'Mantente al día con las últimas innovaciones en tecnología y marketing digital. Lee sobre Next.js, servidores modernos, HTML5, desarrollo web y más en nuestro blog.',
+  twitterLabel1: 'Tiempo de lectura',
+  twitterData1: '4 minutos',
+})
+
+</script>
+
+<template>
+
+<Title
+    title="Blog"
+    description="Entérate de los últimos artículos de Vanguardia relacionados al mundo del marketing digital y tecnología." />
+
+  <BlogArticle />
+</template>
